a66bb2232096c13f949b40b3791a69ba1b35c828
boguman <91239254+boguman@users.noreply.github.com>
2021-10-30 13:51:54 +0900

    Update Message.py

--- Message.py
+++ Message.py
@@ -1,1 +1,16 @@
+!pip install twilio
 
+from twilio.rest import Client
+
+account_sid = 'AC4d43aa42bcd517487a133de0ba3e8ed3'
+auth_token = '70703a627e06f6db77393a27137b8555'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                  body="우편이 도착하였습니다.", #메세지 내용
+                  from_='+12055286335',          #송신 번호
+                  to='+821092071520'             #수신 번호
+                )
+
+print(message.sid)
